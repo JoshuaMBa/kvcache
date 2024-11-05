fd42331594ee30d6af1f5927409f7a6d4bb93898
Joshua Baehring <josh.baehring@gmail.com>
2024-11-05 07:01:21 +0000

    [kv] Complete part B1.
    Implemented basic client-shard handler.

--- lab4/kv/client.go
+++ lab4/kv/client.go
@@ -2,6 +2,10 @@ package kv
 
 import (
 	"context"
+	"cs426.yale.edu/lab4/kv/proto"
+	"errors"
+	"fmt"
+	"sync"
 	"time"
 
 	"github.com/sirupsen/logrus"
@@ -10,6 +14,7 @@ import (
 type Kv struct {
 	shardMap   *ShardMap
 	clientPool ClientPool
+	mu         sync.Mutex
 
 	// Add any client-side state you want here
 }
@@ -23,14 +28,43 @@ func MakeKv(shardMap *ShardMap, clientPool ClientPool) *Kv {
 	return kv
 }
 
+func (kv *Kv) getNode(shard int) (string, error) {
+	nodes := kv.shardMap.NodesForShard(shard)
+	if len(nodes) == 0 {
+		return "", errors.New(fmt.Sprintf("no node hosts shard %v", shard))
+	}
+
+	return nodes[0], nil
+}
+
 func (kv *Kv) Get(ctx context.Context, key string) (string, bool, error) {
 	// Trace-level logging -- you can remove or use to help debug in your tests
 	// with `-log-level=trace`. See the logging section of the spec.
 	logrus.WithFields(
 		logrus.Fields{"key": key},
 	).Trace("client sending Get() request")
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
 
-	panic("TODO: Part B")
+	shard := GetShardForKey(key, kv.shardMap.NumShards())
+	node, err := kv.getNode(shard)
+	if err != nil {
+		return "", false, err
+	}
+
+	kvClient, err := kv.clientPool.GetClient(node)
+	if err != nil {
+		return "", false, err
+	}
+
+	response, err := kvClient.Get(ctx, &proto.GetRequest{
+		Key: key,
+	})
+	if err != nil {
+		return "", false, err
+	}
+
+	return response.Value, response.WasFound, nil
 }
 
 func (kv *Kv) Set(ctx context.Context, key string, value string, ttl time.Duration) error {
